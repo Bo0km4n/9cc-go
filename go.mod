d0349287a960f15ccee645f457810cb091f35bf0
Bo0km4n <katsuya_kawabe@cyberagent.co.jp>
2019-05-24 12:03:08 +0900

    Update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Bo0km4n/9cc-go
+
+go 1.12
--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println(".intel_syntax noprefix")
+	fmt.Println(".global main")
+	fmt.Println("main:")
+	fmt.Println("	mov rax, 42")
+	fmt.Println("	ret")
 }
