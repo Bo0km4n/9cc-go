d4d136f93c49c2ca31cba1f7b8c3eab94093fee3
Bo0km4n <katsuya_kawabe@cyberagent.co.jp>
2019-05-24 12:12:03 +0900

    Update

---
+++ Makefile
@@ -0,0 +1,4 @@
+9cc:
+	go build -o 9cc
+clean:
+	rm -f 9cc *.o *~ tmp*
--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println(".intel_syntax noprefix")
-	fmt.Println(".global main")
-	fmt.Println("main:")
-	fmt.Println("	mov rax, 42")
-	fmt.Println("	ret")
+	argv := os.Args[1]
+
+	in, _ := strconv.Atoi(argv)
+	fmt.Printf(".intel_syntax noprefix\n")
+	fmt.Printf(".global main\n")
+	fmt.Printf("main:\n")
+	fmt.Printf("	mov rax, %d\n", in)
+	fmt.Printf("	ret\n")
 }
